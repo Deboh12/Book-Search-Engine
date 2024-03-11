220937bdd40876da8e1df17b71732eb7b8db2ded
Deboh12 <deborah.alexander12@yahoo.com>
2024-03-11 17:17:22 -0500

    mutations.js and app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,31 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 import Navbar from './components/Navbar';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+
+// Set up Apollo Client
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+  headers: {
+    authorization: localStorage.getItem('id_token') || '',
+  },
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<SearchBooks />} />
+          <Route path="/saved" element={<SavedBooks />} />
+        </Routes>
+      </Router>
+    </ApolloProvider>
   );
 }
 
